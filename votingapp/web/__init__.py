e53517244794b619b053f7a074fc5b9019ae2e24
19fn <fcabrera@gmail.com>
2022-09-05 22:14:16 -0300

    Add sensible data

--- votingapp/web/__init__.py
+++ votingapp/web/__init__.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder="templates")
 
 # SENSIBLE
-server_name = ""
-database_name = ""
-server_admin_login_name = ""
-server_admin_login_password = ""
+server_name = "mysql-server-devtest-eu01.mysql.database.azure.com"
+database_name = "devtest"
+server_admin_login_name = "fnc1_admin@mysql-server-devtest-eu01"
+server_admin_login_password = "bKS1s4X7Sb9hdOGP"
 
 # Database string connection
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{server_admin_login_name}:{server_admin_login_password}@{server_name}:3306/{database_name}"
